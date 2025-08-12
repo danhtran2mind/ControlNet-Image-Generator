3ddc8631e3b0db0be4521243f89fc58b7899307f
danhtran2mind <cprmmb99@gmail.com>
2025-08-12 09:25:38 +0700

    Change some codes

--- src/controlnet_image_generator/train.py
+++ src/controlnet_image_generator/train.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 # Add diffusers examples/controlnet to sys.path to import train_controlnet
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", 
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", 
                              "third_party", "diffusers", "examples", "controlnet"))
 
 from train_controlnet import main
